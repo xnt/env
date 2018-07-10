052ce21e8b87aa1c6ecd4dfc24457d95e9da7eec
Vicente Plata <xientyto@gmail.com>
2018-07-10 17:54:58 -0400

    :building_construction: Add VSCode files

--- backup_files.sh
+++ backup_files.sh
@@ -2,10 +2,16 @@
 #
 # Backup relevant files
 
-files=( ".vimrc" ".bash_profile" ".bashrc" )
+files=( ".vimrc" ".bash_profile" ".bashrc" "Library/Application Support/Code/User/settings.json")
 
 for f in "${files[@]}"
 do
   :
+  echo "Backing up ${HOME}/${f}"
   cp "${HOME}/${f}" .
 done
+
+echo "Writing VSCode extensions list"
+rm -f vscode
+echo "# Restore with code --install-extension {extension id}" >> vscode
+code --list-extensions >> vscode
